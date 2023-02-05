910373a656d6303a7863b7ff4a8ee66c85c72841
Kosmo00 <vladiotero19101916@gmail.com>
2023-02-05 00:04:49 -0500

    adding buttons scripts

--- Scenes/Puzzle3.gd
+++ Scenes/Puzzle3.gd
@@ -1,33 +1,41 @@
 extends Node2D
 onready var plants = $"Node2D/Lab Tiles"
 
-const spawnpoints = [Vector2(1950,1360)]
+#bridges
+onready var puente1 = $"Puente1"
+onready var puente4 = $"Puente4"
+onready var puente5 = $"Puente5"
+onready var puente7 = $"Puente7"
+onready var puente9 = $"Puente9"
+onready var puente10 = $"Puente10"
+onready var puente11 = $"Puente11"
+onready var puente13 = $"Puente13"
+onready var puente15 = $"Puente15"
+
+
+const spawnpoints = [Vector2(-583, 4999)]
 
 var bridges = [] # [Node2D, ...]
 var colisions = [] # [colision, ...]
 var connections = [] # [[colision_number, colision_number], ...]
+var three_element_max = []
 
 func _ready():
+	bridges = [
+		puente1, puente4, puente5, puente7, puente9, puente10, puente11, puente13, puente15
+	]
+	for i in bridges.size():
+		bridges[i].hide()
 	var button = get_node("items/ID Card/Interactive Area")
 	#button.connect("button_pressed", self, "manage_bridges")
 
 
-func manage_bridges(enable: Array, disable: Array):
-	# enable give us an array of bridges (and conections) indexes
-	# disable too
-	enable_bridges(enable)
-	disable_bridges(disable)
-
-func enable_bridges(enable: Array):
-	for i in enable.size():
-		bridges[i].show()
-		colisions[connections[i][0]].disabled = true;
-		colisions[connections[i][1]].disabled = true;
-	pass
-
-func disable_bridges(disable: Array):
-	for i in disable.size():
+func manage_bridges(enable: int):
+	three_element_max.push_back(enable)
+	if three_element_max.size() > 3:
+		three_element_max.pop_front()
+	for i in bridges.size():
 		bridges[i].hide()
-		colisions[connections[i][0]].disabled = false;
-		colisions[connections[i][1]].disabled = false;
+	for i in three_element_max.size():
+		bridges[three_element_max[i]].show()
 	pass
