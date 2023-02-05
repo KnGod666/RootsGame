ee62b3b8febd19a0577fe0493d53609901a403ab
KnGod666 <ianrvr@estudiantes.uci.cu>
2023-02-04 20:15:46 -0500

    Diolog works more or less

--- Scripts/Room1.gd
+++ Scripts/Room1.gd
@@ -1,7 +1,9 @@
 extends Node2D
 
+signal change_scene
+
 const spawnpoints = [Vector2(-500,-1600),Vector2(-3700,2700)]
 
 func _on_Room2_TP_area_entered(area):
 	WorldFlags.entrance = 0
-	get_tree().change_scene("res://Scenes/Room2.tscn")
+	emit_signal("change_scene","res://Scenes/Room2.tscn")
